d28f70cc9f838e939c9a1b9c4d2a07d5890dd349
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-09-10 02:38:53 +0300

    Dependencies and packages installation

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-    filename: 'test-task-2.js',
+    filename: 'test-task-1.js',
     path: path.resolve(__dirname, 'build'),
   },
 };
