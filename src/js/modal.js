c6627bd62a560bc8b1d013a1f16dc21dba5a1364
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-09-10 06:33:14 +0300

    Adds functions for modal window opening and closing

---
+++ src/js/modal.js
@@ -0,0 +1,57 @@
+import { alert } from '@pnotify/core/dist/PNotify.js';
+import { Stack } from '@pnotify/core';
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+
+
+const refs = {
+    openModalBtn: document.querySelector('.open-modal-btn'),
+    modalWindow: document.querySelector('.modal__window'),
+    closeModalIcon: document.querySelector('[data-action="close-lightbox"]'),
+    cancelActionBtn: document.querySelector('[data-action="cancel-action"]'),
+    confirmActionBtn: document.querySelector('[data-action="confirm-action"]'),
+}
+
+function openModalWindow (event) {
+    event.preventDefault();
+    
+    refs.modalWindow.classList.add('is-open');
+}
+
+function closeModalWindow () {
+    refs.modalWindow.classList.remove('is-open');
+}
+
+function confirmAction () {
+    refs.modalWindow.classList.remove('is-open');
+
+    alert({
+        text: 'DONE',
+        type: 'error',
+        delay: 2000,
+        stack: new Stack({
+            dir1: 'up',
+        }),
+    })
+}
+
+function escBttnAction(event) {
+    if (event.keyCode === 27) {
+        refs.modalWindow.classList.remove('is-open');
+    }
+};
+
+
+function closeModalByOverlay(event) {
+    console.log(event.target.nodeName);
+    // if (event.target.nodeName !== 'DIV') {
+    //     refs.modalWindow.classList.remove('is-open');
+    // }
+}
+
+refs.openModalBtn.addEventListener('click', openModalWindow);
+refs.closeModalIcon.addEventListener('click', closeModalWindow);
+refs.cancelActionBtn.addEventListener('click', closeModalWindow);
+refs.confirmActionBtn.addEventListener('click', confirmAction);
+refs.modalWindow.addEventListener('click', closeModalByOverlay);
+document.addEventListener('keydown', escBttnAction);
